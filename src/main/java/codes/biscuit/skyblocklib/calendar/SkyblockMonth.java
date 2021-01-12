4d6ee80d62b9d959780b87bfcb825911337aaa46
Tominous <18189490+Tominous@users.noreply.github.com>
2021-01-12 10:58:42 +0000

    Update SkyblockMonth.java

--- src/main/java/codes/biscuit/skyblocklib/calendar/SkyblockMonth.java
+++ src/main/java/codes/biscuit/skyblocklib/calendar/SkyblockMonth.java
@@ -11,9 +11,9 @@ public enum SkyblockMonth {
     EARLY_SUMMER("Early Summer"),
     SUMMER("Summer"),
     LATE_SUMMER("Late Summer"),
-    EARLY_FALL("Early Fall"),
-    FALL("Fall"),
-    LATE_FALL("Late Fall");
+    EARLY_FALL("Early Autumn"),
+    FALL("Autumn"),
+    LATE_FALL("Late Autumn");
 
     private String scoreboardString;
 
